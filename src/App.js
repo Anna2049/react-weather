feebad0891acd461d38b513347137574739486f2
Anna2049 <85588122+Anna2049@users.noreply.github.com>
2023-02-27 02:28:48 +0100

    added link for checking homeworks

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import WeatherSearch from "./WeatherSearch";
 function App() {
   return (
     <div className="App">
+      <a
+        href="https://github.com/Anna2049/react-weather"
+        target="blank"
+        rel="noopener noreferrer"
+      >
+        GitHub link
+      </a>
       <h1>Weather Search Engine</h1>
       <WeatherSearch />
     </div>
